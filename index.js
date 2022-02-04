73702b00bf4614c3573ed4c4b213a51f2d2671aa
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-02-04 10:14:08 -0800

    adding stuff

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 // const allModels = require("./models")
-// const  User = allModels.User
-// const  Joke = allModels.Joke
 const {User,Joke,Comment} = require("./models")
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,14 +12,14 @@ app.use(express.json());
 
 app.get("/api/users",(req,res)=>{
     User.findAll({
-        include:[Joke]
+        include:[Joke,Comment]
     }).then(users=>{
         res.json(users)
     })
 })
 app.get("/api/jokes",(req,res)=>{
     Joke.findAll({
-        include:[User]
+        include:[User,Comment]
     }).then(users=>{
         res.json(users)
     }).catch(err=>{
